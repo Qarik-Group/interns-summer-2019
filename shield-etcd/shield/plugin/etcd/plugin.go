7575aa4330e1e64babc0b3f6e87e13fede511259
Sriniketh Varma <srinikethvarma22@gmail.com>
2019-06-19 10:54:50 -0400

    Etcd plugin in shield validate code implemented

--- shield-etcd/shield/plugin/etcd/plugin.go
+++ shield-etcd/shield/plugin/etcd/plugin.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	fmt "github.com/jhunt/go-ansi"
 
 	"github.com/starkandwayne/shield/plugin"
 )
@@ -16,6 +17,19 @@ func main() {
 			Target: "yes",
 			Store:  "no",
 		},
+		Example: `
+{
+  "endpoints" : "https://192.168.42.45:2379,https://192.168.42.46:2379,https://192.168.42.47:2379"   # REQUIRED
+
+  "auth"  : false				# is role based or cert based auth enabled on the etcd cluster
+  "user"  : "admin",            # username for role based authentication
+  "pass"  : "p@ssw0rd"          # password for role based authentication
+}
+`,
+		Defaults: `
+{
+}
+`,
 		Fields: []plugin.Field{
 			plugin.Field{
 				Mode:     "target",
@@ -28,9 +42,18 @@ func main() {
 			},
 			plugin.Field{
 				Mode:    "target",
+				Name:    "auth",
+				Type:    "bool",
+				Title:   "Authentication",
+				Help:    "Is role based or cert based authentication enabled on the etcd cluster",
+				Example: "false",
+			},
+			plugin.Field{
+				Mode:    "target",
 				Name:    "user",
 				Type:    "string",
 				Help:    "Username for role based authentication",
+				Title:   "Username",
 				Example: "admin",
 			},
 			plugin.Field{
@@ -38,6 +61,7 @@ func main() {
 				Name:    "pass",
 				Type:    "string",
 				Help:    "Password for role based authentication",
+				Title:   "Password",
 				Example: "p@ssw0rd",
 			},
 		},
@@ -48,41 +72,118 @@ func main() {
 
 type EtcdPlugin plugin.PluginInfo
 
-type EtcdConnectionInfo struct {
-	Host string
-	Path string
+type EtcdConfig struct {
+	etcdEndpoints  string
+	Authentication bool
+	Username       string
+	Password       string
 }
 
 func (p EtcdPlugin) Meta() plugin.PluginInfo {
 	return plugin.PluginInfo(p)
 }
 
+func getEtcdConfig(endpoint plugin.ShieldEndpoint) (*EtcdConfig, error) {
+	etcdEndpoint, err := endpoint.StringValue("endpoints")
+	if err != nil {
+		return nil, err
+	}
+
+	auth, err := endpoint.BooleanValueDefault("auth", false)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := endpoint.StringValueDefault("user", "")
+	if err != nil {
+		return nil, err
+	}
+
+	pass, err := endpoint.StringValueDefault("pass", "")
+	if err != nil {
+		return nil, err
+	}
+
+	return &EtcdConfig{
+		etcdEndpoints:  etcdEndpoint,
+		Authentication: auth,
+		Username:       user,
+		Password:       pass,
+	}, nil
+}
+
 func (p EtcdPlugin) Validate(endpoint plugin.ShieldEndpoint) error {
-	fmt.Println("Validate function called")
-	return plugin.UNIMPLEMENTED
+	var (
+		s    string
+		auth bool
+		err  error
+		fail bool
+	)
+	s, err = endpoint.StringValue("endpoints")
+	if err != nil {
+		fmt.Printf("@R{\u2717 endpoints  %s}\n", err)
+		fail = true
+	} else {
+		fmt.Printf("@G{\u2713 endpoints}  files in @C{%s} will be backed up\n", s)
+	}
+
+	auth, err = endpoint.BooleanValueDefault("auth", false)
+	if err != nil {
+		fmt.Printf("@R{\u2717 authentication  %s}\n", err)
+		fail = true
+	} else if auth {
+		fmt.Printf("@G{\u2713 auth} authentication is enabled\n")
+	} else {
+		fmt.Printf("@G{\u2713 auth} authentication is disabled\n")
+	}
+
+	if auth {
+		s, err = endpoint.StringValueDefault("user", "")
+		if err != nil {
+			fmt.Printf("@R{\u2717 user  %s}\n", err)
+			fail = true
+		} else if s == "" {
+			fmt.Printf("@G{\u2717 user} auth was enabled but username was not provided\n")
+			fail = true
+		} else {
+			fmt.Printf("@G{\u2713 user} auth will be done with username @C{%s}\n", s)
+		}
+
+		s, err = endpoint.StringValueDefault("pass", "")
+		if err != nil {
+			fmt.Printf("@R{\u2717 pass  %s}\n", err)
+			fail = true
+		} else if s == "" {
+			fmt.Printf("@R{\u2717 pass} auth was enabled but password was not provided\n")
+			fail = true
+		} else {
+			fmt.Printf("@G{\u2713 user} auth will be done with password @C{%s}\n", s)
+		}
+	}
+
+	if fail {
+		return fmt.Errorf("fs: invalid configuration")
+	}
+
+	return nil
 }
 
 func (p EtcdPlugin) Backup(endpoint plugin.ShieldEndpoint) error {
-	fmt.Println("Backup function called")
 	return plugin.UNIMPLEMENTED
 }
 
 func (p EtcdPlugin) Restore(endpoint plugin.ShieldEndpoint) error {
-	fmt.Println("Restore function called")
 	return plugin.UNIMPLEMENTED
 }
 
 func (p EtcdPlugin) Store(endpoint plugin.ShieldEndpoint) (string, int64, error) {
-	fmt.Println("Store function called")
 	return "", 0, plugin.UNIMPLEMENTED
 }
 
 func (p EtcdPlugin) Retrieve(endpoint plugin.ShieldEndpoint, file string) error {
-	fmt.Println("Retrieve function called")
 	return plugin.UNIMPLEMENTED
 }
 
 func (p EtcdPlugin) Purge(endpoint plugin.ShieldEndpoint, key string) error {
-	fmt.Println("Purge function called")
 	return plugin.UNIMPLEMENTED
 }
